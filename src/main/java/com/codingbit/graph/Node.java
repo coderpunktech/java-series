f473b0c63d2e508ddd6f9da2d3466e7fde22f357
Massimo Presta <mexassi85@gmail.com>
2022-02-21 17:40:26 +1100

    removing visited state from node

--- src/main/java/com/codingbit/graph/Node.java
+++ src/main/java/com/codingbit/graph/Node.java
@@ -1,7 +1,5 @@
 package com.codingbit.graph;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -13,56 +11,13 @@ public class Node {
      * Describes the way to identify a particular Node
      */
     private final String name;
-    /**
-     * All the edges for this node
-     */
-    private final List<Edge> edges;
-    /**
-     * Describes whether the node has been visited or not
-     */
-    private boolean visited;
 
     public Node(final String name) {
         this.name = name;
-        this.visited = false;
-        this.edges = new ArrayList<>();
-    }
-
-    public Node add(final Edge edge) {
-        this.edges.add(edge);
-        return this;
     }
 
     public String getName() {
         return name;
-    }
-
-    public List<Edge> getEdges() {
-        return edges;
-    }
-
-    public boolean isVisited() {
-        return visited;
-    }
-
-    /**
-     * Mark a node as visited
-     *
-     * @return this
-     */
-    public Node visit() {
-        this.visited = true;
-        return this;
-    }
-
-    /**
-     * Mark a node as unvisited
-     *
-     * @return this
-     */
-    public Node unvisit() {
-        this.visited = false;
-        return this;
     }
 
     @Override
@@ -70,20 +25,18 @@ public class Node {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Node node = (Node) o;
-        return visited == node.visited && Objects.equals(name, node.name) && Objects.equals(edges, node.edges);
+        return Objects.equals(name, node.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, edges, visited);
+        return Objects.hash(name);
     }
 
     @Override
     public String toString() {
         return "Node{" +
                 "name='" + name + '\'' +
-                ", edges=" + edges +
-                ", visited=" + visited +
                 '}';
     }
 }
